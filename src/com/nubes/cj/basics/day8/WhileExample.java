5011b3a39c561671d83baa00bc87181a945622de
tanviavula <57667839+tanviavula@users.noreply.github.com>
2020-01-08 21:21:58 +0530

    Added code to get input from the user

--- src/com/nubes/cj/basics/day8/WhileExample.java
+++ src/com/nubes/cj/basics/day8/WhileExample.java
@@ -1,12 +1,14 @@
 package com.nubes.cj.basics.day8;
-
+import java.util.Scanner;
 // This program will print numbers from 1 to 10
 public class WhileExample {
 	
 	public static void main(String[] args) {
-			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the number value:");
+			int num = sc.nextInt();
 			int i = 1;
-			while(i <= 10) {
+			while(i <= num) {
 				System.out.println(i);
 				i++;
 			}

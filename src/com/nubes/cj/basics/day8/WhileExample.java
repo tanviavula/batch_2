499de80cb3020ebdd6f66e8c6a6d6dbb60861ffd
unknown <lakshman.avula@outlook.com>
2020-01-08 21:22:53 +0530

    Added extra information

--- src/com/nubes/cj/basics/day8/WhileExample.java
+++ src/com/nubes/cj/basics/day8/WhileExample.java
@@ -4,11 +4,12 @@ package com.nubes.cj.basics.day8;
 public class WhileExample {
 	
 	public static void main(String[] args) {
-			
+			//This would be better if user gets input from the user
 			int i = 1;
 			while(i <= 10) {
 				System.out.println(i);
 				i++;
 			}
+
 	}
 }

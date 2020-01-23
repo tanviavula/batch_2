7e6c629ab1cad2e34117c4b9cf7450465dae75ba
LakshmanAvula <lakshman.avula@outlook.com>
2020-01-23 22:12:21 +0530

    Added Wrapper sample test

--- src/com/nubes/cj/basics/HelloWorld.java
+++ src/com/nubes/cj/basics/HelloWorld.java
@@ -3,5 +3,11 @@ package com.nubes.cj.basics;
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Java world");
+		Integer a = new Integer(20);
+		Integer b = new Integer(20);
+		int c = 20;
+		System.out.println(a == c);
+		System.out.println(a == b);
+		System.out.println(a.equals(b));
 	}
 }

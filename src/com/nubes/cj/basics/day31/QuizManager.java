8b9f3242c00a5290e4559feb742192d425838b92
M Rajesh <m.rajeshrajz@gmail.com>
2020-02-18 20:08:08 +0530

    added quiz program

--- src/com/nubes/cj/basics/day31/QuizManager.java
+++ src/com/nubes/cj/basics/day31/QuizManager.java
@@ -18,61 +18,80 @@ public class QuizManager {
 		String[] userAnswers = new String[quiz.getQCount()];
 		String[] answers = new String[quiz.getQCount()];
 		int c = 0;
-		for(Question q:quiz.getQuestions()) {
+		for (Question q : quiz.getQuestions()) {
 			System.out.println(q.getQdata());
-			System.out.println("A."+q.getOption1());
-			System.out.println("B."+q.getOption2());
-			System.out.println("C."+q.getOption3());
-			System.out.println("D."+q.getOption4());
+			System.out.println("A." + q.getOption1());
+			System.out.println("B." + q.getOption2());
+			System.out.println("C." + q.getOption3());
+			System.out.println("D." + q.getOption4());
 			System.out.println("Enter your answer:");
-			
+
 			userAnswers[c] = sc.nextLine();
 			answers[c] = q.getAnswer();
 			c++;
 		}
 		int ccount = 0;
 		int wcount = 0;
-		for(int i=0;i<answers.length;i++) {
-			if(answers[i].equals(userAnswers[i])) {
+		for (int i = 0; i < answers.length; i++) {
+			if (answers[i].equalsIgnoreCase(userAnswers[i])) {
 				ccount++;
-			}else {
+			} else {
 				wcount++;
 			}
 		}
-		System.out.println("Total question: "+quiz.getQCount());
-		System.out.println("No of correct: "+ccount);
-		System.out.println("No fo wrongs: "+wcount);
-		
+		System.out.println("Total question: " + quiz.getQCount());
+		System.out.println("No of correct: " + ccount);
+		System.out.println("No fo wrongs: " + wcount);
+
 	}
 
 	private static Quiz createAndGetQuiz() {
-		
-		String data = "1 + 2  = ?,23,34,3,6,C-1 + 2  = ?,23,34,3,6,C-1 + 2  = ?,23,34,3,6,C";
+
+		String data = "Who won the IPL final 2019?=Chennai Super Kings,Kolkata Knight Riders,Mumbai Indians,Royal Challengers Bangalore,C-How many times has India won the ICC World Cup?=2,1,4,3,A-Who got FIFA Best Player 2019 Award?=Neymar,Messi,Ronaldo,Luka Mordic,B";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the title of Quiz:");
 		String title = sc.nextLine();
 		Quiz quiz = new Quiz(title);
+
+		String questions[] = data.split("-");
+//		for (String s : questions) {
+//
+//		}
+//
+//		for (int i = 0; i < quiz.getQuestions().length; i++) {
+//
+//			System.out.println("Enter the question " + (i + 1) + " Details :");
+//			System.out.println("Enter the Question :");
+//			String qdata = sc.nextLine();
+//			System.out.println("Enter the option1:");
+//			String option1 = sc.nextLine();
+//
+//			System.out.println("Enter the option2:");
+//			String option2 = sc.nextLine();
+//
+//			System.out.println("Enter the option3:");
+//			String option3 = sc.nextLine();
+//
+//			System.out.println("Enter the option4:");
+//			String option4 = sc.nextLine();
+//
+//			System.out.println("Enter the Answer:(A,B,C,D)");
+//			String answer = sc.nextLine();
+//			Question question = new Question(qdata, option1, option2, option3, option4, answer);
+//			quiz.getQuestions()[i] = question;
+//
+//		}
+
 		for (int i = 0; i < quiz.getQuestions().length; i++) {
-			System.out.println("Enter the question " + (i + 1) + " Details :");
-			System.out.println("Enter the Question :");
-			String qdata = sc.nextLine();
-			System.out.println("Enter the option1:");
-			String option1 = sc.nextLine();
-
-			System.out.println("Enter the option2:");
-			String option2 = sc.nextLine();
-
-			System.out.println("Enter the option3:");
-			String option3 = sc.nextLine();
-
-			System.out.println("Enter the option4:");
-			String option4 = sc.nextLine();
-
-			System.out.println("Enter the Answer:(A,B,C,D)");
-			String answer = sc.nextLine();
+			String ques = questions[i];
+			String qdata = ques.split("=")[0];
+			String option1 = ques.split("=")[1].split(",")[0];
+			String option2 = ques.split("=")[1].split(",")[1];
+			String option3 = ques.split("=")[1].split(",")[2];
+			String option4 = ques.split("=")[1].split(",")[3];
+			String answer = ques.split("=")[1].split(",")[4];
 			Question question = new Question(qdata, option1, option2, option3, option4, answer);
 			quiz.getQuestions()[i] = question;
-
 		}
 		return quiz;
 	}
